7db84f5330fd9cc1284a72c0c1c7214ff114ab05
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-18 13:21:04 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
-
-def get_storage_type():
-    return 'db' if getenv('HBNB_TYPE_STORAGE') == 'db' else 'file'
-
-def get_class_by_name(class_name):
-    classes = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "State": State,
-        "City": City,
-        "Place": Place,
-        "Amenity": Amenity,
-        "Review": Review
-    }
-    return classes.get(class_name)
-
-# This line should be after all the functions to prevent circular import
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from os import getenv
 
-storage_t = get_storage_type()
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+storage.reload()
