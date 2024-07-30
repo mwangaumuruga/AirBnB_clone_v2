3d104d3c8df88243c707fede242657efb09ac385
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-30 12:04:29 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+
 import models
 from models.city import City
 import shlex
@@ -16,8 +18,11 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
+        
 
     @property
     def cities(self):
