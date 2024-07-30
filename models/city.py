6ef6c1f76ae8ac788452b373483876f81ea0b72e
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-30 11:48:52 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 
