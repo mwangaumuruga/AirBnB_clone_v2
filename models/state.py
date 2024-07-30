a981a87dee3133adc6212b8a15d65b6abf09cf3a
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-30 13:09:19 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,11 +6,6 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 
-import models
-from models.city import City
-import shlex
-
-
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
@@ -22,19 +17,18 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
-        
+    else:
+        @property
+        def cities(self):
+            """
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+            """
+            import models
+            from models.city import City
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+                  return city_list   
+                   
+                
