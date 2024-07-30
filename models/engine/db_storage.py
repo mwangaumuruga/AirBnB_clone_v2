22572a2b864bab23cedac0f05d58bd978f7f25f7
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-30 15:13:39 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
-import os
-from sqlalchemy import create_engine
+"""
+#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
+new class for sqlAlchemy
+"""
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv 
 from sqlalchemy import (create_engine)
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
+from models.base_model import Base, BaseModel
 from models.state import State
 from models.city import City
 from models.user import User
@@ -16,70 +16,61 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = os.getenv('HBNB_ENV')
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
-        
-        if HBNB_ENV == 'test':
-            from models.base_model import Base
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        dic = {}
+        all_classes = {}
+        model_classes = [State, City, User, Place, Review, Amenity]
         if cls:
-            if type(cls) is str:
-                cls = eval(cls)
             query = self.__session.query(cls)
-            for elem in query:
-                key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
-        else:
-            lista = [State, City, User, Place, Review, Amenity]
-            for clase in lista:
-                query = self.__session.query(clase)
-                for elem in query:
-                    key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-        return (dic)
+            for v in query:
+                key = "{}.{}".format(v.__class__.__name__, v.id)
+                all_classes[key] = v
+            return all_classes
+
+        for cls in model_classes:
+            queries = self.__session.query(cls).all()
+            for query in queries:
+                key = "{}.{}".format(query.__class__.__name__, query.id)
+                all_classes[key] = query
+        return all_classes
 
     def new(self, obj):
-        """add a new element in the table
-        """
         self.__session.add(obj)
 
     def save(self):
-        """save changes
-        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table
-        """
         if obj:
-            self.session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
-        """configuration
-        """
-        Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
-        self.__session = Session()
+        try:
+            # Create metadata
+            Base.metadata.create_all(self.__engine)
+            # Create a new session
+            self.__session = scoped_session(sessionmaker(
+                bind=self.__engine, expire_on_commit=True))
+        except Exception as e:
+            # Handle any errors that occur during
+            # metadata creation or session initialization
+            print(f"Error occurred during reload: {e}")
 
     def close(self):
-        """ calls remove()
-        """
         self.__session.close()
