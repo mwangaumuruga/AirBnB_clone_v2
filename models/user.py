2ded4597c68647b28097de28e11800f67d07814c
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-07-31 13:36:52 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
